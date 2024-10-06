af50c5467dc5de0659d4253b4c8b47cf7f0b71b8
ydev1992 <flyn.rider0x@gmail.com>
2024-10-06 12:45:55 -0400

    wallet connect

--- src/index.tsx
+++ src/index.tsx
@@ -1,63 +1,16 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import "./index.css";
-import App from "./App";
-import reportWebVitals from "./reportWebVitals";
-import { connectorsForWallets } from "@rainbow-me/rainbowkit";
-import {
-  rainbowWallet,
-  metaMaskWallet,
-  phantomWallet,
-} from "@rainbow-me/rainbowkit/wallets";
-import "@rainbow-me/rainbowkit/styles.css";
-import { getDefaultConfig, RainbowKitProvider } from "@rainbow-me/rainbowkit";
-import { WagmiProvider } from "wagmi";
-import {
-  mainnet,
-  polygon,
-  optimism,
-  arbitrum,
-  base,
-} from 'wagmi/chains';
-import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
-import { createConfig } from 'wagmi';
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
 
-const connectors = connectorsForWallets(
-  [
-    {
-      groupName: "Recommended",
-      wallets: [phantomWallet, rainbowWallet, metaMaskWallet],
-    },
-  ],
-  {
-    appName: "My RainbowKit App",
-    projectId: "YOUR_PROJECT_ID",
-  }
-);
-
-const queryClient = new QueryClient();
-// const config = createConfig(
-//   connectors
-// );
-const config = getDefaultConfig({
-  appName: 'My RainbowKit App',
-  projectId: 'YOUR_PROJECT_ID',
-  chains: [mainnet, polygon, optimism, arbitrum, base],
-  ssr: true, // If your dApp uses server side rendering (SSR)
-});
 
 const root = ReactDOM.createRoot(
-  document.getElementById("root") as HTMLElement
+  document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <WagmiProvider config={config}>
-      <QueryClientProvider client={queryClient}>
-        <RainbowKitProvider>
-          <App />
-        </RainbowKitProvider>
-      </QueryClientProvider>
-    </WagmiProvider>
+    <App />
   </React.StrictMode>
 );
 
